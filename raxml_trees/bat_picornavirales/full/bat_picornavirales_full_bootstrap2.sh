00701974b5e9f22254556946b7ce55b7b3adf7f9
Gwenddolen Kettenburg <gkettenburg@uchicago.edu>
2023-08-15 11:22:37 -0500

    bat picornavirales raxml output

---
+++ raxml_trees/bat_picornavirales/full/bat_picornavirales_full_bootstrap2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=batpicorna_full_boot2
+#SBATCH --partition=broadwl
+#SBATCH --output=batpicorna_full_boot2.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --ntasks-per-node=8
+#SBATCH --time=36:00:00
+
+module load vim/7.4
+module load emacs/25.1
+module load python/3.6
+module load java/1.8.0_121
+module load cmake/3.15.1
+
+raxml-ng-mpi --bootstrap --msa bat_picornavirales_full_3kb_align.fasta --model GTR+G4 --prefix T14  --seed 23 --threads auto{8}
