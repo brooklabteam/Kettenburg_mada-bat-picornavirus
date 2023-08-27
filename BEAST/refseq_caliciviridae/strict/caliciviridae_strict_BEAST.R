d8b38a21b3caf510435c977d32b2be910f1de045
Gwenddolen Kettenburg <gkettenburg@uchicago.edu>
2023-08-27 13:36:14 -0500

    beast R script

--- BEAST/refseq_caliciviridae/strict/caliciviridae_strict_BEAST.R
+++ BEAST/refseq_caliciviridae/strict/caliciviridae_strict_BEAST.R
@@ -10,21 +10,15 @@ library(lubridate)
 library(treeio)
 
 
-#make Bayesian timetree from Astrovirus strict molecular clock model
+#make Bayesian timetree from caliciviridae strict molecular clock model
 
 #first, read in the tree
 
-homewd= "/Users/sophiahorigan/Documents/GitHub/Mada-Bat-Astro/"
+homewd= "/Users/gwenddolenkettenburg/Desktop/developer/mada-bat-picornavirus/"
 
-setwd(paste0(homewd, "/Bayesian-trees"))
+setwd(paste0(homewd, "/BEAST/refseq_caliciviridae/strict"))
 
-
-tree <-  read.beast(file = paste0(homewd, "Bayesian-trees/astro-full-beast-MCC"))
-#tree <-  read.beast(file = paste0(homewd, "Bayesian-trees/TESTT"))
-
-
-#test <- root(tree, which(tree@phylo$tip.label == "NC_002470_1985-07-15"))
-
+tree <-  read.beast(file = paste0(homewd, "/BEAST/refseq_caliciviridae/strict/caliciviridae_strict_mean_tree"))
 
 treedat <- cbind.data.frame(tip_name = tree@phylo$tip.label)
 treedat$beast_name <-treedat$tip_name
@@ -33,22 +27,39 @@ treedat$beast_name <-treedat$tip_name
 #tree$node.label <- round(tree$posterior,2)
 #treedat <- cbind.data.frame(tip_name = tree$tip.label)
 treedat$accession_num <- sapply(strsplit(treedat$tip_name, "_"), function(x) x[[1]])
-treedat$accession_num[treedat$accession_num=="NC"] <- c("NC_001943", "NC_002469", "NC_004579", "NC_011400", "NC_013060", "NC_013443", "NC_015935", "NC_016155", "NC_016896", "NC_018702", "NC_019026", "NC_019027", "NC_019028", "NC_019494", "NC_022249", "NC_023629", "NC_023630", "NC_023631", "NC_023632", "NC_023636", "NC_023674", "NC_023675", "NC_024297", "NC_024472", "NC_024498", "NC_024701", "NC_025346", "NC_025379", "NC_026814", "NC_027711", "NC_030922", "NC_033792", "NC_033821", "NC_034974", "NC_036583", "NC_037655")
-treedat$accession_num[treedat$accession_num=="F"] <- c("OQ606244")
+treedat$accession_num[treedat$accession_num=="NC"] <- c("NC_000940","NC_001481","NC_001543","NC_001959","NC_002551","NC_002615","NC_004064",
+                                                        "NC_004541","NC_004542","NC_006269","NC_006554","NC_006875","NC_007916","NC_008311",
+                                                        "NC_008580","NC_010624","NC_011050","NC_011704","NC_012699","NC_017936","NC_019712",
+                                                        "NC_024031","NC_024078","NC_025676","NC_027026","NC_027122","NC_029645","NC_029646",
+                                                        "NC_029647","NC_030793","NC_031324","NC_033081","NC_033776","NC_034444","NC_035675",
+                                                        "NC_039475","NC_039476","NC_039477","NC_039897","NC_040674","NC_040876","NC_043512",
+                                                        "NC_043516","NC_044045","NC_044046","NC_044047","NC_044853","NC_044854","NC_044855",
+                                                        "NC_044856","NC_044932","NC_045762")
+treedat$accession_num[treedat$accession_num=="F"] <- c("OQ818319")
 
 #names(treedat)[names(treedat)=="tip_name"] <- "beast_name"
 
-
 #and load data of corresponding tree
 
-dat <- read.csv(file = "astro_full_beast_meta.csv", header = T, stringsAsFactors = F)
-dat$collection_date <- as.Date(dat$collection_date)
+dat <- read.csv(file = "caliciviridae_beast_metadata.csv", header = T, stringsAsFactors = F)
+dat$Collection_Date <- as.Date(dat$Collection_Date)
 
 
-#test 
+colz = c("Sapovirus" = "royalblue3",    "Vesivirus"  = "turquoise1",   "Lagovirus"  = "goldenrod1",   "Norovirus"   = "dodgerblue1" ,   "Calicivirus" = "firebrick1" ,
+         "Salovirus"  = "lightpink1" ,    "Bavovirus"  = "hotpink1" ,  "Minovirus" = "lightskyblue" ,   "Coronavirus"  = "black", "Recovirus"  = "darkorange1", 
+         "Nacovirus"="thistle3", "Nebovirus"="darkorchid4")
 
-mrsd.dat <- max(dat$collection_date)
+#pick order for the labels
+dat$Genus <- factor(dat$Genus, levels = c("Sapovirus" ,  "Vesivirus",   "Lagovirus",   "Norovirus",   "Calicivirus",
+                                          "Salovirus",    "Bavovirus",  "Minovirus",  "Recovirus", "Nacovirus", "Nebovirus", 
+                                          "Coronavirus"))   
+
+dat$novel <- as.factor(dat$novel)
+
+
+mrsd.dat <- max(dat$Collection_Date)
 p1 <- ggtree(tree, mrsd=mrsd.dat)  + theme_tree2()  +geom_nodelab()
+p1
 
 tree.dat <- p1$data
 node.sub <- dplyr::select(tree.dat, node, x)
@@ -57,93 +68,58 @@ names(node.sub) <-  c("node", "nodetime")
 #and 
 head(dat)
 
-dat$clade <- dat$strain
-#need to check these against literature.
-dat$clade[dat$host == "Sus scrofa" ] <- "Porcine AstV" 
-dat$clade[dat$host == "Bos taurus" ] <- "Bovine AstV"
-dat$clade[dat$host == "Homo sapiens" ] <- "Human AstV" 
-dat$clade[dat$host == "Mus musculus" ] <- "Murine AstV" 
-dat$clade[dat$host == "Felis catus" ] <- "Feline AstV" 
-dat$clade[dat$host == "Marmota himalayana" ] <- "Marmot AstV" 
-dat$clade[dat$host == "Ovis aries" ] <- "Bovine AstV" 
-dat$clade[dat$host == "Mouse" ] <- "Murine AstV" 
-dat$clade[dat$host == "Oryctolagus cuniculus" ] <- "Leporine AstV" 
-dat$clade[dat$host == "Budorcas taxicolor tibetana" ] <- "Bovine AstV"
-dat$clade[dat$bat_host==1] <- "Bat AstV"
-dat$clade[dat$host == "Canis lupus familiaris" ] <- "Canine AstV"
-dat$clade[dat$host == "Camelus dromedarius" ] <- "Camel AstV"
-dat$clade[dat$host == "Neogale vison" ] <- "Mink AstV"
-dat$clade[dat$host == "Rattus norvegicus" ] <- "Murine AstV"
-dat$clade[dat$host == "Meleagris gallopavo" ] <- "AvastV"
-
-
-
-
 dat.plot <- merge(treedat, dat, by="accession_num", all.x = T, sort=F)
 
 head(dat.plot)
 dat.plot$new_label = NA
-dat.plot$new_label[!is.na(dat.plot$strain)] <- paste(dat.plot$accession_num[!is.na(dat.plot$strain)], " | ", 
-                                                     dat.plot$strain[!is.na(dat.plot$strain)], " | ", 
-                                                     dat.plot$host[!is.na(dat.plot$strain)], " | ",
-                                                     dat.plot$country[!is.na(dat.plot$strain)], " | ",
-                                                     dat.plot$collection_year[!is.na(dat.plot$strain)])
+dat.plot$new_label[!is.na(dat.plot$Species)] <- paste(dat.plot$accession_num[!is.na(dat.plot$Species)], " | ", 
+                                                     dat.plot$Species[!is.na(dat.plot$Species)], " | ", 
+                                                     dat.plot$source[!is.na(dat.plot$Species)], " | ",
+                                                     dat.plot$country[!is.na(dat.plot$Species)], " | ",
+                                                     dat.plot$collection_year[!is.na(dat.plot$Species)])
 
-dat.plot$new_label[is.na(dat.plot$strain)] <- paste(dat.plot$accession_num[is.na(dat.plot$strain)], " | ", 
-                                                    dat.plot$host[is.na(dat.plot$strain)], " | ",
-                                                    dat.plot$country[is.na(dat.plot$strain)], " | ",
-                                                    dat.plot$collection_year[is.na(dat.plot$strain)])
+dat.plot$new_label[is.na(dat.plot$Species)] <- paste(dat.plot$accession_num[is.na(dat.plot$Species)], " | ", 
+                                                    dat.plot$source[is.na(dat.plot$Species)], " | ",
+                                                    dat.plot$country[is.na(dat.plot$Species)], " | ",
+                                                    dat.plot$collection_year[is.na(dat.plot$Species)])
 
 
 tree@phylo$tip.label <- dat.plot$new_label
 
-dat.sub <- dplyr::select(dat.plot, new_label, collection_date, country, clade)
+dat.sub <- dplyr::select(dat.plot, new_label, Genus, Family, Collection_Date,Host, Country, source, Collection_Year, novel, Isolate, Species)
 head(dat.sub)
-dat.sub$clade <- as.factor(dat.sub$clade)
 
-dat.sub$novel = "no"
-dat.sub$novel[dat.sub$country=="Madagascar"] <- "yes"
+# dat.sub$novel = "no"
+# dat.sub$novel[dat.sub$country=="Madagascar"] <- "yes"
+# 
+# colz2 = c('yes' =  "yellow", 'no' = "white")
 
-colz2 = c('yes' =  "yellow", 'no' = "white")
+dat.sub$Host[dat.sub$Host==0] <- "Non-bat host"
+dat.sub$Host[dat.sub$Host==1] <- "Bat host"
+dat.sub$Host <- as.factor(dat.sub$Host)
+shapez = c("Bat host" =  17, "Non-bat host" = 19)
+colz2 = c('1' =  "yellow", '0' = "white")
 
-p3 <-ggtree(tree, mrsd=mrsd.dat, size=.8) %<+% dat.sub +
-  geom_tippoint(aes(color=clade), size=4) +
-  scale_color_manual(values=c('Bat AstV' = "#0CB702", 'Bovine AstV' = "#00BFC4", 'Camel AstV' = "#00A9FF", 'Canine AstV' = "#8494FF", 'Feline AstV' = "#C77CFF", 'Human AstV' = "#ED68ED", 'Leporine AstV' = "#FF68A1", 'Marmot AstV' = "#E68613", 'Mink AstV' = "#CD9600", 'Murine AstV' = "#ABA300", 'Porcine AstV' = "#7CAE00")) +
-  scale_fill_manual(values=c('Bat AstV' = "#0CB702", 'Bovine AstV' = "#00BFC4", 'Camel AstV' = "#00A9FF", 'Canine AstV' = "#8494FF", 'Feline AstV' = "#C77CFF", 'Human AstV' = "#ED68ED", 'Leporine AstV' = "#FF68A1", 'Marmot AstV' = "#E68613", 'Mink AstV' = "#CD9600", 'Murine AstV' = "#ABA300", 'Porcine AstV' = "#7CAE00")) +
-  #geom_nodelab(size=2.5,nudge_x = -21, nudge_y = .7) +
-  # geom_nodelab(aes(label=new.nodel.lab), size=4,nudge_x = -55, nudge_y = -1,  color="firebrick", fontface=2, geom="label", fill="white") +
-  #geom_nodelab(size=1.8,nudge_x = -.05, nudge_y = .7) +
-  #geom_treescale(fontsize=2.5) + 
-  theme_tree2() +
-  theme(legend.position = c(.01,.7), plot.margin = unit(c(.2,24,3,3), "lines"), legend.title = element_blank()) +
+#plot tree
+p1 <-ggtree(tree, mrsd=mrsd.dat, size=.8) %<+% dat.sub+
+  geom_tippoint(aes(color=Genus, shape=Host), size=3,stroke=0,show.legend = T) +  
+  scale_fill_manual(values=colz) +
+  scale_color_manual(values=colz)+
+  scale_shape_manual(values=shapez) +
+  new_scale_fill() +
+  #theme_tree2() +
   coord_cartesian(clip = "off") + 
-  geom_nodepoint(aes(fill=posterior), shape=21, color="black", size=3, stroke=.1) +
+  geom_nodepoint(aes(fill=posterior), shape=21, color="black", size=3, stroke=.1)+
   scale_fill_continuous(low="yellow", high="red") +
   guides(fill_continuous = guide_legend(order = 2),col = guide_legend(order = 1)) +
-  scale_x_continuous(breaks=c(1620, 1720, 1820, 1920, 2020),
-                     labels=c(400, 300, 200, 100, 0)) +
-  xlab("years to MRCA") +
-  ggnewscale::new_scale_fill() +
-  geom_tiplab(aes(fill = novel), geom = "label", label.size = 0, alpha=.3,  show.legend=F, size=3) + scale_fill_manual(values=colz2) 
-#xlim(c(0,6))
+  theme(legend.position = "left", 
+        legend.direction = "vertical",
+        legend.text = element_text(size=8), 
+        legend.key.size = unit(0.3, "cm"))+
+  scale_x_continuous(breaks=c(1400, 1600, 1800, 2000),
+                     labels=c(623, 423, 223, 23)) +
+  xlab("Years to MRCA")
 
-p3
-
-
-ggsave(file = paste0(homewd, "/final-figures/Fig4.png"),
-       units="mm",  
-       width=90, 
-       height=60, 
-       #limitsize = F,
-       scale=3)#, 
+p1
 
 
-
-
-#calculate how long ago Rousettus split from Eidolon
-orig.date <- round(node.sub$nodetime[33],0)
-
-nodeRous <- MRCA(tree, which(tree@phylo$tip.label =="OQ606244  |  Bat astrovirus  |  Rousettus madagascariensis  |  Madagascar  |  2018"), which(tree@phylo$tip.label =="MG693176  |  Bat astrovirus  |  Eidolon helvum  |  Cameroon  |  2013"))
-Rous.date <- round(node.sub$nodetime[nodeRous],0)
-
-Rous.date <- Rous.date - 2018
