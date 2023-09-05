79422c3a7bdd4686f3aac76f128d3067d8dab7ee
Gwenddolen Kettenburg <gkettenburg@uchicago.edu>
2023-09-05 11:50:08 -0500

    africa picornaviridae tree scripts

--- raxml_trees/africa_picornaviridae/africa_picornaviridae_raxml/africa_picornaviridae_over1kb_tree_infer.sh
+++ raxml_trees/africa_picornaviridae/africa_picornaviridae_raxml/africa_picornaviridae_over1kb_tree_infer.sh
@@ -13,4 +13,4 @@ module load python/3.6
 module load java/1.8.0_121
 module load cmake/3.15.1
 
-raxml-ng-mpi --msa africa_picornaviridae_over1kb_align.fasta --model GTR+G4 --prefix T2  --seed 1 --threads auto{8}
+raxml-ng-mpi --msa africa_picornaviridae_over1kb_align.fasta --model GTR+I+G4 --prefix T2  --seed 1 --threads auto{8}
