be8a58232546cdd627bf0f7d8a081962e0b96ac9
Gwenddolen Kettenburg <gkettenburg@uchicago.edu>
2023-09-13 15:34:57 -0500

    supplemental refseq picornaviridae and caliciviridae

--- raxml_trees/picornaviridae_calicivirdae_refseq_trees.R
+++ raxml_trees/picornaviridae_calicivirdae_refseq_trees.R
@@ -16,8 +16,8 @@ library(cowplot)
 ##picornaviridae first
 homewd= "/Users/gwenddolenkettenburg/Desktop/developer/mada-bat-picornavirus/"
 
-##Africa bat picornaviridae
-setwd(paste0(homewd,"/raxml_trees/africa_picornaviridae/africa_picornaviridae_raxml/P2P3"))
+##Picornaviridae
+setwd(paste0(homewd,"/raxml_trees/picornaviridae_refseq"))
 
 #load the tree
 tree <-  read.tree("T10.raxml.supportFBP") 
@@ -27,7 +27,7 @@ rooted.tree <- root(tree, which(tree$tip.label == "NC_030886.1"))
 plot(rooted.tree)
 
 #load tree data prepared from elsewhere
-dat <- read.csv(("africa_picornaviridae_P2P3_metadata.csv"), header = T, stringsAsFactors = F)
+dat <- read.csv(("picornaviridae_refseq_metadata.csv"), header = T, stringsAsFactors = F)
 head(dat)
 
 #check that your metadata matches your tree data
@@ -180,6 +180,7 @@ p1 <- ggtree(rooted.tree) %<+% tree.dat + geom_tippoint(aes(color=Genus, shape=H
   scale_color_manual(values=colz)+
   scale_shape_manual(values=shapez) +
   new_scale_fill() +
+  guides(colour = guide_legend(ncol = 1))+
   geom_tiplab(aes(fill = novel, show.legend=F), geom = "label", family="Helvetica", label.size = 0, label.padding = unit(0, "lines"), alpha=.4, size=2, nudge_x=0.05) +
   guides(fill="none")+#
   scale_fill_manual(values=colz2) +
@@ -212,9 +213,6 @@ p1.1 <- p1  %<+% p0.dat +
 p1.1
 
 
-#Export uncollapsed 30x10 portrait PDF
-
-
 ##Get the clade numbers so we can collapse unnnecesary clades
 ggtree(rooted.tree) + geom_text(aes(label=node), hjust=-.3)
 
@@ -226,8 +224,9 @@ p2 <- ggtree(rooted.tree) %<+% tree.dat + geom_tippoint(aes(color=Genus, shape=H
   scale_fill_manual(values=colz) +
   scale_color_manual(values=colz)+
   scale_shape_manual(values=shapez) +
+  guides(colour = guide_legend(ncol = 1))+
   new_scale_fill() +
-  geom_tiplab(aes(fill = novel, show.legend=F), geom = "label", family="Helvetica", label.size = 0, label.padding = unit(0, "lines"), alpha=.4, size=3, nudge_x=0.05) +
+  geom_tiplab(aes(fill = novel, show.legend=F), geom = "label", Genus="Helvetica", label.size = 0, label.padding = unit(0, "lines"), alpha=.4, size=3, nudge_x=0.05) +
   guides(fill="none")+#
   scale_fill_manual(values=colz2) +
   geom_treescale(fontsize=4, x=0,y=-3, linesize = .5) +
@@ -236,24 +235,61 @@ p2 <- ggtree(rooted.tree) %<+% tree.dat + geom_tippoint(aes(color=Genus, shape=H
         legend.text = element_text(size=8), 
         legend.key.size = unit(0.3, "cm")) +
   xlim(c(0,14))+
-  geom_cladelabel(node = 395, label = "Apthovirus/Bos taurus clade",offset=0.1, fontsize = 3, color="black") +
-  geom_cladelabel(node = 594, label = "Enterovirus/Human and non-human primate clade",offset=0.1, fontsize = 3, color="black")
+  geom_cladelabel(node = 261, label = "Enterovirus",offset=0.3, fontsize=3, color="black") +
+  geom_cladelabel(node = 235, label = "Hepatovirus", offset=0.3,fontsize=3, color="black") +
+  geom_cladelabel(node = 213, label = "Avisivirus", offset=0.3, fontsize=3, color="black") +
+  geom_cladelabel(node = 216, label = "Limnipivirus/Potamipivirus", offset=0.3,fontsize=3, color="black") +
+  geom_cladelabel(node = 205, label = "Parechovirus/Shanbavirus/Avihepatovirus", offset=0.3,fontsize=3, color="black") +
+  geom_cladelabel(node = 336, label = "Ampivirus", offset=0.3, fontsize=3, color="pink2") +
+  geom_cladelabel(node = 190, label = "Malagasivirus/Tottorivirus/Hunnivirus", offset=0.3, fontsize=3, color="black") +
+  geom_cladelabel(node = 188, label = "Mosavirus", offset=0.3,fontsize=3, color="black") +
+  geom_cladelabel(node = 185, label = "Cosavirus", fontsize=3, offset=0.3, color="black") +
+  geom_cladelabel(node = 342, label = "Megrivirus/Ailurivirus",fontsize=3, offset=0.3, color="black") +
+  geom_cladelabel(node = 346, label = "Cardiovirus",fontsize=3, offset=0.3, color="4") +
+  geom_cladelabel(node = 354, label = "Cardiovirus/Senecavirus",fontsize=3, offset=0.3, color="black") +
+  geom_cladelabel(node = 351, label = "Apthovirus", offset=0.3,fontsize=3, color="black") +
+  geom_cladelabel(node = 353, label = "Bopivirus/Erbovirus", offset=0.3, fontsize=3, color="black")+
+  geom_cladelabel(node = 333, label = "Rosavirus", offset=0.3, fontsize=3,color="black") +
+  geom_cladelabel(node = 332, label = "Harkavirus/Dicipivirus",fontsize=3, offset=0.3, color="black") +
+  geom_cladelabel(node = 326, label = "Oscivirus/Livupivirus/Rafivirus",fontsize=3,offset=0.3, color="black") +
+  geom_cladelabel(node = 323, label = "Megrivirus/Parechovirus", fontsize=3, offset=0.3,color="black") +
+  geom_cladelabel(node = 295, label = "Gallivirus/Megrivirus",fontsize=3, offset=0.3,color="black") +
+  geom_cladelabel(node = 303, label = "Passerivirus/Sicinvirus", offset=0.3, fontsize=3, color="black")+
+  geom_cladelabel(node = 320, label = "Salivirus/Sakobuvirus",fontsize=3,offset=0.3, color="black")
 p2
 
 
-
 #collapse the labeled clades
-p3<-collapse(p2, 395)+geom_point2(aes(subset=(node==395)), size=3, shape=22, fill="darkgoldenrod2")
-p12<-collapse(p3, 594)+geom_point2(aes(subset=(node==594)), size=3, shape=22, fill="cadetblue2")
-p12
+p3<-collapse(p2, 261)+geom_point2(aes(subset=(node==261)), size=3, shape=22, fill="cadetblue2")
+p4<-collapse(p3, 235)+geom_point2(aes(subset=(node==235)), size=3, shape=22, fill="cadetblue3")
+p5<-collapse(p4, 213)+geom_point2(aes(subset=(node==213)), size=3, shape=22, fill="hotpink2")
+p6<-collapse(p5, 216)+geom_point2(aes(subset=(node==216)), size=3, shape=22, fill="deeppink4")
+p7<-collapse(p6, 205)+geom_point2(aes(subset=(node==205)), size=3, shape=22, fill="deeppink4")
+p8<-collapse(p7, 336)+geom_point2(aes(subset=(node==336)), size=3, shape=22, fill="pink2")
+p9<-collapse(p8, 190)+geom_point2(aes(subset=(node==190)), size=3, shape=22, fill="deeppink4")
+p10<-collapse(p9, 188)+geom_point2(aes(subset=(node==188)), size=3, shape=22, fill="lightblue4")
+p11<-collapse(p10, 185)+geom_point2(aes(subset=(node==185)), size=3, shape=22, fill="darkgoldenrod4")
+p12<-collapse(p11, 342)+geom_point2(aes(subset=(node==342)), size=3, shape=22, fill="deeppink4")
+p13<-collapse(p12, 346)+geom_point2(aes(subset=(node==346)), size=3, shape=22, fill="cadetblue1")
+p14<-collapse(p13, 354)+geom_point2(aes(subset=(node==354)), size=3, shape=22, fill="deeppink4")
+p15<-collapse(p14, 351)+geom_point2(aes(subset=(node==351)), size=3, shape=22, fill="darkgoldenrod2")
+p16<-collapse(p15, 353)+geom_point2(aes(subset=(node==353)), size=3, shape=22, fill="deeppink4")
+p17<-collapse(p16, 333)+geom_point2(aes(subset=(node==333)), size=3, shape=22, fill="hotpink1")
+p18<-collapse(p17, 332)+geom_point2(aes(subset=(node==332)), size=3, shape=22, fill="deeppink4")
+p19<-collapse(p18, 326)+geom_point2(aes(subset=(node==326)), size=3, shape=22, fill="deeppink4")
+p20<-collapse(p19, 323)+geom_point2(aes(subset=(node==323)), size=3, shape=22, fill="deeppink4")
+p21<-collapse(p20, 295)+geom_point2(aes(subset=(node==295)), size=3, shape=22, fill="deeppink4")
+p22<-collapse(p21, 303)+geom_point2(aes(subset=(node==303)), size=3, shape=22, fill="deeppink4")
+p23<-collapse(p22, 320)+geom_point2(aes(subset=(node==320)), size=3, shape=22, fill="deeppink4")
+p23
 
 
 ##add bootstrap values to this tree
-p12.dat <- p12$data
-p12.dat$Bootstrap <- NA
-Bootstrap<-p12.dat$Bootstrap[(length(tree.dat$tip_label)+1):length(p12.dat$label)] <- as.numeric(p12.dat$label[(length(tree.dat$tip_label)+1):length(p12.dat$label)])#fill with label
+p23.dat <- p23$data
+p23.dat$Bootstrap <- NA
+Bootstrap<-p23.dat$Bootstrap[(length(tree.dat$tip_label)+1):length(p23.dat$label)] <- as.numeric(p23.dat$label[(length(tree.dat$tip_label)+1):length(p23.dat$label)])#fill with label
 
-p12.1 <- p12  %<+% p12.dat + 
+picornaviridae <- p23  %<+% p23.dat + 
   ggnewscale::new_scale_fill() + 
   geom_nodepoint(aes(fill=Bootstrap, show.legend = T), shape=21, stroke=0)+
   scale_fill_continuous(low="yellow", high="red", limits=c(0,100))+
@@ -263,16 +299,14 @@ p12.1 <- p12  %<+% p12.dat +
         legend.text = element_text(size=8),
         legend.title = element_text(size=8),
         legend.key.size = unit(0.3, "cm"))
-p12.1
+picornaviridae
 
 
 
 
 
-
-
-##Africa bat caliciviridae
-setwd(paste0(homewd,"/raxml_trees/africa_caliciviridae/africa_caliciviridae_raxml"))
+##Refseq caliciviridae
+setwd(paste0(homewd,"/raxml_trees/caliciviridae_refseq"))
 
 #load the tree
 tree <-  read.tree("T1.raxml.supportFBP") 
@@ -282,15 +316,15 @@ rooted.tree <- root(tree, which(tree$tip.label == "NC_030886.1"))
 plot(rooted.tree)
 
 #load tree data prepared from elsewhere
-dat <- read.csv(("africa_caliciviridae_metadata.csv"), header = T, stringsAsFactors = F)
+dat <- read.csv(("caliciviridae_refseq_metadata.csv"), header = T, stringsAsFactors = F)
 head(dat)
 
 #check that your metadata matches your tree data
 setdiff(rooted.tree$tip.label, dat$tip_label)
 #check for duplicates
 setdiff(dat$tip_label, rooted.tree$tip.label) #no duplicates
-nrow(dat) #45
-length(tree$tip.label) #44
+nrow(dat) #58
+length(tree$tip.label) #58
 
 #check subgroup names
 unique(dat$Genus)
@@ -464,15 +498,15 @@ p1 <- ggtree(rooted.tree) %<+% tree.dat + geom_tippoint(aes(color=Genus, shape=H
   scale_color_manual(values=colz)+
   scale_shape_manual(values=shapez) +
   new_scale_fill() +
-  geom_tiplab(aes(fill = novel, show.legend=F), geom = "label", Genus="Helvetica", label.size = 0, label.padding = unit(0, "lines"), alpha=.4, size=2,  nudge_x=0.1) +
-  guides(fill="none")+#
+  geom_tiplab(aes(fill = novel, show.legend=F), geom = "label", Genus="Helvetica", label.size = 0, label.padding = unit(0, "lines"), alpha=.4, size=3,  nudge_x=0.1) +
+  guides(fill="none", shape="none")+#
   scale_fill_manual(values=colz2) +
   geom_treescale(fontsize=4, x=0,y=-3, linesize = .5) +
   theme(legend.position = "left", 
         legend.direction = "vertical",
         legend.text = element_text(size=8), 
         legend.key.size = unit(0.3, "cm")) +
-  xlim(c(0,8))
+  xlim(c(0,14))
 
 p1
 
@@ -483,7 +517,7 @@ p0.dat$Bootstrap <- NA
 Bootstrap<-p0.dat$Bootstrap[(length(tree.dat$tip_label)+1):length(p0.dat$label)] <- as.numeric(p0.dat$label[(length(tree.dat$tip_label)+1):length(p0.dat$label)])#fill with label
 
 #add bootstrap values to original plot
-p1.1 <- p1  %<+% p0.dat + 
+caliciviridae <- p1  %<+% p0.dat + 
   ggnewscale::new_scale_fill() + 
   geom_nodepoint(aes(fill=Bootstrap, show.legend = F), shape=21, stroke=0)+
   scale_fill_continuous(low="yellow", high="red", limits=c(0,100))+
@@ -493,21 +527,15 @@ p1.1 <- p1  %<+% p0.dat +
         legend.text = element_text(size=8),
         legend.title = element_text(size=8),
         legend.key.size = unit(0.3, "cm"))
-p1.1
-
-
-
-
-
-
+caliciviridae
 
 
 
 ##Make the figure
-supp<-plot_grid(iflaviridae,secoviridae, ncol=2, rel_widths = c(1.2,1), 
+supp<-plot_grid(picornaviridae,caliciviridae, ncol=2, rel_widths = c(1.2,1), 
                 rel_heights = c(1,1),labels = "AUTO",
                 align="hv", axis="b", label_size = 23)
 supp
 
-#export 20x12 inch landscape
+#export 20x12 inch landscape PDF
 
