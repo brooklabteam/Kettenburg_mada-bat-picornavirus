a8a768a0a3ff8f0db65d110151bfafbf2a2c4fcc
Gwenddolen Kettenburg <gkettenburg@uchicago.edu>
2024-06-13 15:34:05 -0500

    polymerase summary tree

---
+++ raxml_trees/polymerase_region_summary_tree/polymerase_summary_bootstrap3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=poly_boot3
+#SBATCH --partition=broadwl
+#SBATCH --output=poly_boot3.out
+#SBATCH --nodes=1
+#SBATCH --ntasks=7
+#SBATCH --ntasks-per-node=7
+#SBATCH --time=36:00:00
+
+module load vim/7.4
+module load emacs/25.1
+module load python/3.6
+module load java/1.8.0_121
+module load cmake/3.15.1
+
+raxml-ng-mpi --bootstrap --msa polymerase_summary_align_trim.fasta --model GTR+G4 --prefix T5  --seed 2 --threads auto{7}
